b511ffec3777c4fb11704c20b15245acec6cd416
Hector Fabio Spitia <hspitia@gmail.com>
2009-04-07 08:19:37 -0500

    Union GUI con rutinas de segmentacion

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,15 +1,17 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindowClass)
 {
   inDir="";
   outDir="";
-  vitrinitaColor = QColor(140,140,140);
-  leptinitaColor = QColor(110,110,110);
-  inertinitaColor = QColor(140,140,140);
-  piritaColor = QColor(140,140,140);
+  vitrinitaColor = QColor(0,0,255);
+  leptinitaColor = QColor(255,0,0);
+  inertinitaColor = QColor(255,255,255);
+  piritaColor = QColor(255,255,0);
+  objSegmentation = new Segmentation();
   ui->setupUi(this);
 }
 
@@ -33,7 +35,51 @@ void MainWindow::on_browseOutBtn_clicked()
 
 void MainWindow::on_executeBtn_clicked()
 {
-    
+    QDir dir(inDir);
+	dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+	QString dirImage = "";
+		QFileInfoList list = dir.entryInfoList();
+		for (int i = 0; i < list.size(); ++i) 
+		{
+			QFileInfo fileInfo = list.at(i);
+			QString name = fileInfo.fileName();
+			dirImage = inDir + name;
+			
+			inImage = new Image();
+			inImage->readImage(dirImage);
+			inGrayImage = new Image();
+			objSegmentation->colorToGrayscale(inImage,inGrayImage);
+			
+			if(name.indexOf("Fluo")>=0)
+			{
+			  outImage = new Image(*inImage);
+			  int time = QString(name.at(name.indexOf("X")+2)).toInt();
+			  //cout<<"Fluo "<<time<<endl;
+			  if(time == 2)
+			  {
+				objSegmentation->segment1(inGrayImage,outImage,vitrinitaColor,leptinitaColor,piritaColor);
+			  }
+			  else if(time == 3)
+			  {
+			  	objSegmentation->segment2(inGrayImage,outImage,vitrinitaColor,leptinitaColor,piritaColor);
+			  }
+			}
+			else if(name.indexOf("Refle")>=0)
+			{
+				objSegmentation->segment3(inGrayImage,outImage,inertinitaColor);
+				//cout<<"Refle "<<endl;
+				outImage->saveImage(outDir + "a" + QString(name.at(1)) + QString(name.at(2)));
+				cout<<qPrintable("procesando..." + outDir + "a" + QString(name.at(1)) + QString(name.at(2)))<<endl;
+				delete outImage;
+				outImage = NULL;
+			}
+			
+			delete inImage;
+			inImage = NULL;
+			delete inGrayImage;
+			inGrayImage = NULL;
+		}
+		cout<<"Listo"<<endl;
 }
 
 void MainWindow::on_exitBtn_clicked()
